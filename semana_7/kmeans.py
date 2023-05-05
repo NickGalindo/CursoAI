b9289a438ff79ae27c5bf6c897f3f86bba1e3104
NickGalindo <ngalindoc@unal.edu.co>
2023-05-04 23:09:36 -0500

    semana 7

--- semana_7/kmeans.py
+++ semana_7/kmeans.py
@@ -4,13 +4,19 @@ import matplotlib.pyplot as plt
 
 
 from sklearn.cluster import KMeans
+from torch.cuda import init
 
 
 # TODO: define data in class
+data = [[1, 1], [0, 1], [0, 1], [-1, -1],
+        [10, 11], [9, 10], [11, 11]]
+data = np.array(data)
 
 convergence = 100
 loss,  center_points, center_arrays = [], [], []
 centers = np.random.uniform((np.min(data), np.max(data)), size=(3, 2)) # Hyperparam k is defined here
+
+pprint(centers)
 
 while(convergence > 0.01):
     center_array = np.zeros(len(data))
@@ -19,20 +25,22 @@ while(convergence > 0.01):
         closest_center = np.argmin([np.linalg.norm(x - center) for center in centers]) 
         center_array[i] = closest_center
 
-    new_centers = np.array([np.mean(data[center_array == i], axis=0) for i in range(num_centers)])    
+    new_centers = np.array([np.mean(data[center_array == i], axis=0) for i in range(3)])    
     convergence = np.sum(np.abs(new_centers - centers))
     centers = new_centers
+    pprint(new_centers)
 
     center_points.append(new_centers)
     center_arrays.append(center_array)
 
-plt.scatter([c[0] for c in centers], [c[1] for c in centers], s="5", c="r")
-plt.scatter([d[0] for d in data], [d[1] for d in data], s="1")
+pprint(centers)
+plt.scatter([c[0] for c in centers], [c[1] for c in centers], s=5, c="r")
+plt.scatter([d[0] for d in data], [d[1] for d in data], s=1)
 plt.show()
 
 
 # With sklearn making it ezpz
-model = KMeans(n_clusters=3)
+model = KMeans(n_clusters=3, init="k-means++")
 model.fit(data)
 
 labels = model.predict(data)
